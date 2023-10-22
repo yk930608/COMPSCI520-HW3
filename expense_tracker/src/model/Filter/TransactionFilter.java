3e7f79ec4c945df379edcbc99b988246208f12ce
Heather Conboy <hconboy@cs.umass.edu>
2023-10-21 20:50:47 -0400

    Update TransactionFilter.java
    Added comments about applying the Strategy design pattern.

--- expense_tracker/src/model/Filter/TransactionFilter.java
+++ expense_tracker/src/model/Filter/TransactionFilter.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import model.Transaction;
 
+/**
+ * The TransactionFilter supports filtering the transaction list.
+ *
+ * NOTE) The Strategy design pattern is being applied. This is the Strategy interface.
+ */
 public interface TransactionFilter {
 
   public List<Transaction> filter(List<Transaction> transactions);
