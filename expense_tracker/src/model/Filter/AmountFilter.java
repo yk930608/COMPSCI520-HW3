5672b5aba74e5fbd95748f6a42be1cb6ec0a202b
Heather Conboy <hconboy@cs.umass.edu>
2023-10-21 20:57:11 -0400

    Update AmountFilter.java
    Added comments in the constructor and filter method providing more details about the solution.

--- expense_tracker/src/model/Filter/AmountFilter.java
+++ expense_tracker/src/model/Filter/AmountFilter.java
@@ -10,6 +10,8 @@ public class AmountFilter implements TransactionFilter{
     private double amountFilter;
 
     public AmountFilter(double amountFilter){
+        // Since the AmountFilter constructor is public, 
+        // the input validation needs to be performed again.
         if(!InputValidation.isValidAmount(amountFilter)){
             throw new IllegalArgumentException("Invalid amount filter");
         } else {
@@ -20,6 +22,7 @@ public class AmountFilter implements TransactionFilter{
     public List<Transaction> filter(List<Transaction> transactions){
         List<Transaction> filteredTransactions = new ArrayList<>();
         for(Transaction transaction : transactions){
+            // Your solution could use a different comparison here.
             if(transaction.getAmount() == amountFilter){
                 filteredTransactions.add(transaction);
             }
